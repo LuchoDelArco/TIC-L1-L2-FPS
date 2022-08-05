144ae4055fe56c224b07475c950e7b98acc67e7a
LuchoDelArco <47138072@est.ort.edu.ar>
2022-08-05 11:01:49 -0300

    UpToEx4

---
+++ Assets/Scripts/CarroNets.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarroNets : MonoBehaviour
+{
+	public GameObject netbook;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.gameObject.name == "FPSController")
+		{
+			//if (Input.GetKeyDown(KeyCode.N))
+			//{
+				Instantiate(netbook).transform.position = new Vector3(-3, 1, 4);
+
+			//}
+		}
+	}
+}
+
